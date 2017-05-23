7e1b3365fbdcdaf67c6d2ebf2e931cd38a16865a
Vyaches Afanas <vyaches.afanas@gmail.com>
2017-05-23 14:31:21 +0400

    test

---
+++ Wallet.java
@@ -0,0 +1,15 @@
+
+
+class Wallet{
+
+    Wallet(){
+        Map wal = new HashMap<String, int>;
+    }
+
+    void addMoney(string currency, int amount){
+        wal.put(currency, amount);
+    }
+
+
+
+}
